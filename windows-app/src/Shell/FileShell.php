1199d4aa875646cb9a9f33eaa76eb86d7c83a779
Luis Araujo <ei11059@fe.up.pt>
2015-12-07 02:52:01 +0000

    s

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -8,7 +8,9 @@ class FileShell extends Shell
 {
     public function main()
     {
-	$stuff = "stuf";
+        $this->loadModel('Stocks');
+	$tick_names = $this->Stocks->find()->distinct('tick_name')->toArray();
+	$stuff = implode(",",$tick_names);;
 	$now = Time::now();
         //$this->createFile('/home/demo/files_created_each_minute/'.$now->year.'-'.$now->month.'-'.$now->day.' '.$now->hour.':'.$now->minute.':'.$now->second.'.txt', $stuff);
         $this->createFile('/home/demo/files_created_each_minute/'.$now->i18nFormat('yyyy-MM-dd HH:mm:ss').'.txt', $stuff);
