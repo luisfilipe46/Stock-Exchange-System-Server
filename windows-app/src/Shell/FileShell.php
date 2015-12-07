fac6832e70b44c14882c74f26fb0b5566f37e2db
Luis Araujo <ei11059@fe.up.pt>
2015-12-07 04:00:25 +0000

    a

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -16,11 +16,13 @@ class FileShell extends Shell
         for ($i = 0; $i < sizeof($someStocks); $i++) {
             $response = $http->get('http://download.finance.yahoo.com/d/quotes?f=sl1d1t1v&s='.$someStocks[$i]['tick_name']);
             $tick_name = explode(",",$response->body())[0];
-            $tick_names[] = str_replace("\"", "", $tick_name);
-            $actualValue[] = explode(",",$response->body())[1];
+            $tick_names_and_values = [str_replace("\"", "", $tick_name), explode(",",$response->body())[1]];
+            //$tick_names[] = str_replace("\"", "", $tick_name);
+            //$actualValue[] = explode(",",$response->body())[1];
         }
 
-        $stuff = implode(",",$tick_names);;
+        //$stuff = implode(",",$tick_names);
+        $stuff = implode(",",$tick_names_and_values);
         $now = Time::now();
         $this->createFile('/home/demo/files_created_each_minute/'.$now->i18nFormat('yyyy-MM-dd HH:mm:ss').'.txt', $stuff);
 
