b00d05cffb1960479c32737257e34293152af58f
Luis Araujo <ei11059@fe.up.pt>
2015-12-07 03:55:58 +0000

    d

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -12,13 +12,15 @@ class FileShell extends Shell
     {
         $http = new Client();
         $this->loadModel('Stocks');
-        $tick_names = $this->Stocks->find()->distinct(['tick_name'])->toArray();
-        for ($i = 0; $i < sizeof($tick_names); $i++) {
-            $response = $http->get('http://download.finance.yahoo.com/d/quotes?f=sl1d1t1v&s='.$tick_names[$i]['tick_name']);
-            $responses[] = explode(",",$response->body())[0];
+        $someStocks = $this->Stocks->find()->distinct(['tick_name'])->toArray();
+        for ($i = 0; $i < sizeof($someStocks); $i++) {
+            $response = $http->get('http://download.finance.yahoo.com/d/quotes?f=sl1d1t1v&s='.$someStocks[$i]['tick_name']);
+            $tick_name = explode(",",$response->body())[0];
+            $tick_names[] = str_replace("\"", "", $tick_name);
+            $actualValue[] = explode(",",$response->body())[1];
         }
 
-        $stuff = implode(",",$responses);;
+        $stuff = implode(",",$tick_names);;
         $now = Time::now();
         $this->createFile('/home/demo/files_created_each_minute/'.$now->i18nFormat('yyyy-MM-dd HH:mm:ss').'.txt', $stuff);
 
