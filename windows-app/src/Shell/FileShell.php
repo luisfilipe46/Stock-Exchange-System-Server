4975ddcbf7c35dd7832e43403c1e7a828fcd4d66
Luis Araujo <ei11059@fe.up.pt>
2015-12-07 03:18:03 +0000

    a

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -3,18 +3,22 @@ namespace App\Shell;
 
 use Cake\Console\Shell;
 use Cake\I18n\Time;
+use Cake\Network\Http\Client;
 
 class FileShell extends Shell
 {
     public function main()
     {
+        $http = new Client();
         $this->loadModel('Stocks');
         $tick_names = $this->Stocks->find()->distinct(['tick_name'])->toArray();
-        //$tick_names = $this->Stocks->find();
-        //$tick_names->select(['tick_name'])->distinct(['tick_name'])->toArray();
-        $stuff = implode(",",$tick_names);;
+        for ($i = 0; $i < sizeof($tick_names); $i++) {
+            $response = $http->get('http://finance.yahoo.com/d/quotes?f=sl1d1t1v&s='.$tick_names[$i]['tick_name']);
+            $responses[] = $response.'\n';
+        }
+
+        $stuff = implode(",",$responses);;
         $now = Time::now();
-        //$this->createFile('/home/demo/files_created_each_minute/'.$now->year.'-'.$now->month.'-'.$now->day.' '.$now->hour.':'.$now->minute.':'.$now->second.'.txt', $stuff);
         $this->createFile('/home/demo/files_created_each_minute/'.$now->i18nFormat('yyyy-MM-dd HH:mm:ss').'.txt', $stuff);
 
 
