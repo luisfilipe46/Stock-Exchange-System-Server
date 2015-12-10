c2c38ecfafa9fa2f7f8ff922bc6dc9df3e2d10a9
Luis Araújo <ei11059@fe.up.pt>
2015-12-10 00:24:06 +0000

    w

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -143,8 +143,8 @@ class FileShell extends Shell
         $stocksAffectedMin=array();
 
         for ($i = 0; $i < sizeof($tick_names_and_values); $i++) {
-            $stocksAffectedMax = $this->Stocks->find()->where(['maximum <=' => $tick_names_and_values[$i][1], 'tick_name =' => $tick_names_and_values[$i][0]])->toArray();
-            $stocksAffectedMin = $this->Stocks->find()->where(['minimum >=' => $tick_names_and_values[$i][1], 'tick_name =' => $tick_names_and_values[$i][0]])->toArray();
+            $stocksAffectedMax[] = $this->Stocks->find()->where(['maximum <=' => $tick_names_and_values[$i][1], 'tick_name =' => $tick_names_and_values[$i][0]])->toArray();
+            $stocksAffectedMin[] = $this->Stocks->find()->where(['minimum >=' => $tick_names_and_values[$i][1], 'tick_name =' => $tick_names_and_values[$i][0]])->toArray();
         }
 
         Debugger::dump($stocksAffectedMax);
