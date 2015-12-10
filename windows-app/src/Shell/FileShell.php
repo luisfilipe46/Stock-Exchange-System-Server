717da7561aaa453da2ee1b9a8a76c2cea2ad5607
Luis Araujo <ei11059@fe.up.pt>
2015-12-10 16:56:02 +0000

    a

--- windows-app/src/Shell/FileShell.php
+++ windows-app/src/Shell/FileShell.php
@@ -112,7 +112,9 @@ class FileShell extends Shell
                 'contain' => []
             ]);
             $channelURI = $device['name'];
-            $MyTileXML = '<tile>
+
+            $MyTileXML = '<tile><visual version="2"><binding template="TileSquare150x150Text01" fallback="TileSquareText01"><text id="1">Text Field 1 (larger text)</text><text id="2">Text Field 2</text><text id="3">Text Field 3</text><text id="4">Text Field 4</text></binding></visual></tile>';
+            /*$MyTileXML = '<tile>
   <visual>
     <binding template="TileSquareText01">
       <text id="1">\'.$tick_name.\' (larger text)</text>
@@ -128,7 +130,7 @@ class FileShell extends Shell
       <text id="2">\'.$value.\'</text>
     </binding>
   </visual>
-</tile>';
+</tile>';*/
             $responseToSendMsg = $Notifier->Send($channelURI,$MyTileXML);
             Debugger::dump($responseToSendMsg);
         }
